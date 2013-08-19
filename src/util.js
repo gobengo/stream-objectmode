9a5f181ff7dcd339ddad45bc32857c68b83a2c97
Benjamin Goering <bengoering@gmail.com>
2013-08-19 16:23:52 -0700

    Add Duplex

--- src/util.js
+++ src/util.js
@@ -10,6 +10,22 @@ define(function () {
         a.prototype.constructor = a;
     };
 
+    /**
+     * Extend the prototype of a Function with any keys on Base.prototype
+     * that aren't already on that Functions prototype
+     */
+    exports.prototypallyInherits = function (a, Base) {
+        var baseKeys = exports.keys(Base.prototype),
+            baseKeysLength = baseKeys.length,
+            methodName;
+        for (var i=0; i < baseKeysLength; i++) {
+            methodName = baseKeys[i];
+            if ( ! a.prototype[methodName]) {
+                a.prototype[methodName] = Base.prototype[methodName];
+            }
+        }
+    };
+
     exports.nextTick = (function () {
         if (typeof setImmediate == 'function') {
             return function(f){ setImmediate(f); };
@@ -39,6 +55,44 @@ define(function () {
             };
         }
     }());
+
+    /**
+     * Object.keys shim
+     */
+    exports.keys = Object.keys || (function () {
+        var hasOwnProperty = Object.prototype.hasOwnProperty,
+            hasDontEnumBug = !{toString:null}.propertyIsEnumerable("toString"),
+            DontEnums = [
+                'toString',
+                'toLocaleString',
+                'valueOf',
+                'hasOwnProperty',
+                'isPrototypeOf',
+                'propertyIsEnumerable',
+                'constructor'
+            ],
+            DontEnumsLength = DontEnums.length;
+
+        return function (o) {
+            if (typeof o != "object" && typeof o != "function" || o === null)
+                throw new TypeError("Object.keys called on a non-object");
+
+            var result = [];
+            for (var name in o) {
+                if (hasOwnProperty.call(o, name))
+                    result.push(name);
+            }
+
+            if (hasDontEnumBug) {
+                for (var i = 0; i < DontEnumsLength; i++) {
+                    if (hasOwnProperty.call(o, DontEnums[i]))
+                        result.push(DontEnums[i]);
+                }
+            }
+
+            return result;
+        };
+    })();
 
     return exports;
 });
